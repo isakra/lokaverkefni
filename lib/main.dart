bf14edc7660b228d2841c62434160fe6b9a71df0
isakra <isakrafnsson@gmail.com>
2024-05-27 22:50:52 +0000

    reading json recipes

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,49 @@ import 'package:flutter/material.dart';
 import 'recipes.dart';
 
 class Category {
-  int id;
+  String id;
   String title;
 
   Category(this.id, this.title);
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      json['id'],
+      json['title'],
+    );
+  }
 }
 
 class Tag {
-  int id;
+  String id;
   String title;
 
   Tag(this.id, this.title);
+
+  factory Tag.fromJson(Map<String, dynamic> json) {
+    return Tag(
+      json['id'],
+      json['title'],
+    );
+  }
 }
 
 class Ingredient {
-  int id;
+  String id;
   String title;
 
   Ingredient(this.id, this.title);
+
+  factory Ingredient.fromJson(Map<String, dynamic> json) {
+    return Ingredient(
+      json['id'],
+      json['title'],
+    );
+  }
 }
 
 class Recipe {
-  int id;
+  String id;
   String title;
   String image;
   List<Ingredient> ingredients;
@@ -34,6 +55,20 @@ class Recipe {
 
   Recipe(this.id, this.title, this.image, this.instructions, this.ingredients,
       this.tags, this.category);
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      json['id'],
+      json['title'],
+      json['image'],
+      json['instructions'],
+      (json['ingredients'] as List<dynamic>)
+          .map((e) => Ingredient.fromJson(e))
+          .toList(),
+      (json['tags'] as List<dynamic>).map((e) => Tag.fromJson(e)).toList(),
+      Category.fromJson(json['category']),
+    );
+  }
 }
 
 void main() {
--- lib/recipes.dart
+++ lib/recipes.dart
@@ -1,5 +1,15 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'main.dart';
+
+Future<List<Recipe>> readRecipes() async {
+  final String recipesJson = await rootBundle.loadString('assets/recipes.json');
+  List<dynamic> decoded = jsonDecode(recipesJson);
+  print(decoded);
+  return decoded.map((json) => Recipe.fromJson(json)).toList();
+}
 
 class RecipesScreen extends StatefulWidget {
   const RecipesScreen({Key? key}) : super(key: key);
@@ -9,6 +19,22 @@ class RecipesScreen extends StatefulWidget {
 }
 
 class _RecipesScreenState extends State<RecipesScreen> {
+  List<Recipe> _recipes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadRecipes();
+  }
+
+  Future<void> _loadRecipes() async {
+    List<Recipe> recipes = await readRecipes();
+    setState(() {
+      _recipes = recipes;
+      print(recipes);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
